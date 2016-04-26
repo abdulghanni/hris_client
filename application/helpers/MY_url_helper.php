ae8fa4f585936d079ad2913d8051fccd1e67588e
abdulghanni <abdulghanni2@gmail.com>
2016-04-26 14:01:26 +0700

    dasd

--- application/helpers/MY_url_helper.php
+++ application/helpers/MY_url_helper.php
@@ -12,10 +12,15 @@ if ( ! function_exists('assets_url'))
     function assets_url($uri = '')
     {
         $CI =& get_instance();
+        $is_pdf = $this->uri->segment(2, 0);
+        $is_pdf = substr($is_pdf, -3);
 
-        $assets_url = $CI->config->item('assets_url');
-
-        return $assets_url . trim($uri, '/');
+        if($is_pdf == "pdf"){
+        	return "http://localhost/hris_client/assets/". trim($uri, '/');
+        }else{
+	        $assets_url = $CI->config->item('assets_url');
+	        return $assets_url . trim($uri, '/');
+	    }
     }
 }
 
