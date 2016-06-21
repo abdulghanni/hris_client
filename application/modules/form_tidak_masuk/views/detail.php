d140dc5e4fc48b09725fb285b69e6b0bfbbcb067
abdulghanni <abdulghanni2@gmail.com>
2016-06-21 10:09:11 +0700

    fix error tmsk

--- application/modules/form_tidak_masuk/views/detail.php
+++ application/modules/form_tidak_masuk/views/detail.php
@@ -585,6 +585,6 @@
 <!--end approve modal HRD--> 
 
 
-              <?php else{
+              <?php }else{
   echo '<div class="col-md-12 text-center">Pengajuan Ini Telah Di Batalkan Oleh Pengaju</div>';
   } ?>
