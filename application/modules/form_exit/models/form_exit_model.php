9a9509e8e6bcaa9db86033c600774a73afe7b656
abdul.ghanni2 <abdul.ghanni2@gmail.com>
2016-10-05 12:00:47 +0700

    update exit_model

--- application/modules/form_exit/models/form_exit_model.php
+++ application/modules/form_exit/models/form_exit_model.php
@@ -63,14 +63,15 @@ class Form_exit_model extends CI_Model {
             }else{
 
             }
-            if($is_admin!=1 && $is_hrd_pusat != 1 & $is_admin_inventaris !=1):
-            if($is_approver == $sess_nik || $is_admin_cabang == 1){
-                $this->db->where_in($this->table.'.user_id', $user);//print_mz($user);
-            }elseif($is_admin!=1 ){
-                 $this->db->where("(users_exit.user_id = '$sess_id'
-                               OR users_exit.user_exit_rekomendasi_id = '$sess_id' OR users_exit.created_by = '$sess_id' OR users_exit.user_app_lv1 = '$sess_nik'  OR users_exit.user_app_lv2 = '$sess_nik'  OR users_exit.user_app_lv3 = '$sess_nik'
-                )",null, false);
-            }
+            // if($is_admin!=1 && $is_hrd_pusat != 1 & $is_admin_inventaris !=1):
+            if($is_admin!=1 && $is_hrd_pusat != 1):
+                if($is_approver == $sess_nik || $is_admin_cabang == 1){
+                    $this->db->where_in($this->table.'.user_id', $user);//print_mz($user);
+                }elseif($is_admin!=1 ){
+                     $this->db->where("(users_exit.user_id = '$sess_id'
+                                   OR users_exit.user_exit_rekomendasi_id = '$sess_id' OR users_exit.created_by = '$sess_id' OR users_exit.user_app_lv1 = '$sess_nik'  OR users_exit.user_app_lv2 = '$sess_nik'  OR users_exit.user_app_lv3 = '$sess_nik'
+                    )",null, false);
+                }
             endif;
 
 
