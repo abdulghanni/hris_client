6794ada066c55601dc3087bd2b710a59375c63c5
andigaluh <andi@komunigrafik.com>
2016-12-01 14:37:14 +0700

    update error message
    update error message

--- application/errors/error_php.php
+++ application/errors/error_php.php
@@ -1,10 +1,13 @@
 <div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
 
-<h4>A PHP Error was encountered</h4>
+<!-- <h4>A PHP Error was encountered</h4>
 
 <p>Severity: <?php echo $severity; ?></p>
 <p>Message:  <?php echo $message; ?></p>
 <p>Filename: <?php echo $filepath; ?></p>
-<p>Line Number: <?php echo $line; ?></p>
+<p>Line Number: <?php echo $line; ?></p> -->
+
+<h4>Telah terjadi kesalahan :</h4>
+<p>Silakan refresh/reload halaman ini atau hubungi HR Pusat/Administrator</p>
 
 </div>
