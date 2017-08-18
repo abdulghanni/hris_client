0655d7f3ac6195741266389d067189e41fee329e
andigaluh <andi@komunigrafik.com>
2017-08-18 10:20:55 +0700

    bug fix pjd approval hrd 1

--- application/modules/form_pjd/views/submit.php
+++ application/modules/form_pjd/views/submit.php
@@ -460,21 +460,21 @@
                               <span class="semi-bold"></span><br/>
                               <span class="small"></span><br/>
                               <span class="semi-bold"></span><br/>
-                              <span class="semi-bold">(HRD)</span>
+                              <span class="semi-bold">(HRD - <?php echo $this->approval->approver('dinas', $td->task_creator)?>)</span>
                             <?php }elseif($td->is_app_hrd == 1){
                              echo ($td->app_status_id_hrd == 1)?"<img class=approval-img src=$approved>": (($td->app_status_id_hrd == 2) ? "<img class=approval-img src=$rejected>"  : (($td->app_status_id_hrd == 3) ? "<img class=approval-img src=$pending>" : "<span class='small'></span><br/>"));?>
                                <span class="small"></span><br/>
                               <span class="small"></span><br/>
                               <span class="semi-bold"><?php echo get_name($td->user_app_hrd)?></span><br/>
                               <span class="small"><?php echo dateIndo($td->date_app_hrd)?></span><br/>
-                              <span class="semi-bold">(HRD)</span>
+                              <span class="semi-bold">(HRD - <?php echo $this->approval->approver('dinas', $td->task_creator)?>)</span>
                             <?php }else{?>
                               <span class="small"></span><br/>
                               <span class="small"></span><br/>
                               <span class="semi-bold"></span><br/>
                               <span class="small"></span><br/>
                               <span class="semi-bold"></span><br/>
-                              <span class="semi-bold">(HRD)</span>
+                              <span class="semi-bold">(HRD - <?php echo $this->approval->approver('dinas', $td->task_creator)?>)</span>
                             <?php } ?>
                           </p>
                         </div>
