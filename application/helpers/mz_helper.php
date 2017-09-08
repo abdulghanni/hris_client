1b04ae7da9051979cb62db32b40e52f6fbfd85c4
andigaluh <andi@komunigrafik.com>
2017-09-08 10:13:58 +0700

    setup kompetensi untuk admin kompetensi

--- application/helpers/mz_helper.php
+++ application/helpers/mz_helper.php
@@ -19,7 +19,7 @@ if (!function_exists('permission')){
         //redirect them to the login page
         redirect('auth/login', 'refresh');
     }
-    elseif (!$CI->ion_auth->is_admin()) //remove this elseif if you want to enable this for non-admins
+    elseif (!$CI->ion_auth->is_admin_kompetensi()&&!$CI->ion_auth->is_admin()) //remove this elseif if you want to enable this for non-admins
     {
         //redirect them to the home page because they must be an administrator to view this
         //return show_error('You must be an administrator to view this page.');
--- application/modules/competency_group/controllers/competency_group.php
+++ application/modules/competency_group/controllers/competency_group.php
@@ -36,7 +36,7 @@ class competency_group extends MX_Controller {
         {
             redirect('auth/login', 'refresh');
         }
-        elseif (!$this->ion_auth->is_admin())
+        elseif (!is_admin_kompetensi()&&!$this->ion_auth->is_admin())
         {
             return show_error('You must be an administrator to view this page.');
         }
